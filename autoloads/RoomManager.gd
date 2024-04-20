99385947c7f4afa063bee3cfd47c26acf6f65184
Leo Feng <thcheetah777@gmail.com>
2024-04-20 16:02:17 -0300

    🚫 restart and esc actions

--- autoloads/RoomManager.gd
+++ autoloads/RoomManager.gd
@@ -12,6 +12,7 @@ var level_4_room: PackedScene = preload("res://rooms/levels/level_4.tscn")
 var level_5_room: PackedScene = preload("res://rooms/levels/level_5.tscn")
 
 var current_room: Room
+var transitioning = false
 
 signal room_changed(room_name: String)
 
@@ -21,9 +22,12 @@ func change_room(room_name: String):
 		printerr(room_name + " is not a valid room")
 		return
 
+	if transitioning: return
+
 	mask.scale = Vector2(2, 2)
 	mask.rotation = 0
 
+	transitioning = true
 	var tween = get_tree().create_tween().set_parallel().set_trans(Tween.TRANS_QUAD)
 	tween.tween_property(mask, "scale", Vector2(0, 0), animation_duration / 2)
 	tween.tween_property(mask, "rotation", PI*2, animation_duration / 2)
@@ -35,8 +39,25 @@ func change_room(room_name: String):
 	get_tree().change_scene_to_packed(scene)
 	room_changed.emit(room_name)
 
+	await Clock.wait(animation_duration / 2)
+
+	transitioning = false
+
 func play_level(level: LevelResource):
+	if transitioning: return
+
 	change_room(level.resource_path.get_file().trim_suffix(".tres"))
 	current_room.color_palette = level.color_palette
 	await Clock.wait(animation_duration / 2)
 	ColorPalette.update_color_palette()
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("esc") and not transitioning:
+		if current_room is LevelRoom:
+			change_room("level_select")
+		else:
+			get_tree().quit()
+
+	if Input.is_action_just_pressed("restart") and current_room is LevelRoom and not transitioning:
+		var current_level = (current_room as LevelRoom)
+		change_room(current_level.level_resource.resource_path.get_file().trim_suffix(".tres"))
