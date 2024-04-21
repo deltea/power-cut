faf4b11e0b7893bf20d343ffd24f68a4a4b703c0
Leo Feng <thcheetah777@gmail.com>
2024-04-21 12:52:18 -0300

    🎁 fix box damping

---
+++ scenes/box.gd
@@ -0,0 +1,9 @@
+class_name Box extends RigidBody2D
+
+@export var drag = 10.0
+
+@onready var area: Area2D = $Area2D
+
+func _process(_delta: float) -> void:
+	var bodies = area.get_overlapping_bodies().filter(func(x): return not x is Box)
+	linear_damp = drag if len(bodies) > 0 else 0.0
