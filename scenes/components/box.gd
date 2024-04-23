19ebdeba46dfd4b416152aeda29cab85c65e29da
Leo Feng <thcheetah777@gmail.com>
2024-04-23 00:13:09 -0300

    🤏 smol

--- scenes/components/box.gd
+++ scenes/components/box.gd
@@ -8,5 +8,5 @@ func _process(_delta: float) -> void:
 	var bodies = area.get_overlapping_bodies().filter(func(x): return not x is Box)
 	linear_damp = drag if len(bodies) > 0 else 0.0
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(_body: Node2D) -> void:
 	AudioManager.play_sound(AudioManager.box)
